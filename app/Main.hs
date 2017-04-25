d9375825df7e11bcbb5bc562a6fa0c8ef58bc179
Ki Yung Ahn <kyagrd@gmail.com>
2017-04-25 21:03:49 +0800

    some scribbling

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
-import Lib
+import           Lib
+
+import           Data.Partition
+import           Data.Set       (singleton)
+
+p :: Partition Int
+p = fromDisjointSets (fmap singleton [1..10])
+
+q1 = joinElems 4 9 p
+p1 = joinElems 2 3 p
+p2 = joinElems 8 4 p1
+p3 = joinElems 2 4 p2
 
 main :: IO ()
-main = someFunc
+-- main = someFunc
+main = do
+  print q1
+  print p1
+  print p2
+  print p3
